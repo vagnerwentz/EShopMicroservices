029bdb273b33ccc89112adb43f5265fb2c18a657
vagnerwentz <wentz.vagner@gmail.com>
2026-02-04 23:47:15 -0300

    feat: Implement GET

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryQueryHandler.cs
@@ -0,0 +1,20 @@
+namespace Catalog.API.Products.GetProductsByCategory;
+
+public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductsByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"{nameof(GetProductsByCategoryQueryHandler)} called with {query}");
+
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(token: cancellationToken);
+        
+        return new GetProductsByCategoryResult(products);
+    }
+}
